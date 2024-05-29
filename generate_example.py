8a6e35c8b6025de5dd6b1b7ce6d5bee7352dae16
erkanadi <erkan.bayram@analog.com>
2024-05-29 16:45:11 -0400

    data directory

--- generate_example.py
+++ generate_example.py
@@ -9,16 +9,16 @@ import time
 # Parameters
 DURATION = 12 # Duration in seconds
 FS = 16000 # Sample frequency in Hz
-GUSTINESS = 10 # Number of speed points. One yields constant wind. High values yield gusty wind (more than 10 can sound unnatural).
+GUSTINESS = 5 # Number of speed points. One yields constant wind. High values yield gusty wind (more than 10 can sound unnatural).
 # WIND_PROFILE_OUTDOOR = np.array([3.45, 6.74, 5.65, 6.34, 4.00,
 #                                  5.88, 3.26, 3.19, 4.78, 4.16,
 #                                  4.67, 4.69, 4.61, 6.53, 6.05]) # Wind speed data example
 
 WIND_PROFILE_OUTDOOR = np.array([ 8.05 , 10.05 , 8.05 , 7.45, 4.32]) # Wind speed data example
-SEED = 1 # Seed for random sequence regeneration
+SEED = 4 # Seed for random sequence regeneration
 
 # Generate wind noise
-wn = wng(fs=FS, duration=DURATION, generate=False,
+wn = wng(fs=FS, duration=DURATION, generate=True,
                 wind_profile=WIND_PROFILE_OUTDOOR,
                 gustiness=GUSTINESS,
                 short_term_var=True, start_seed=SEED)
@@ -31,6 +31,9 @@ wn_signal, wind_profile = wn.generate_wind_noise()
 
 # print('debug 1')
 # wn_signal, wind_profile = wn.generate_wind_noise()
+
+
+
 
 # Save signal in .wav file
 wn.save_signal(wn_signal, filename='./wind_noise_12.wav', num_ch=1, fs=16000)
